0a451ca3b8de54b63ae8c286a79b2e85de9b50af
Areesha Haseeb Khan Suontausta <32227763+AreeshaHaseeb@users.noreply.github.com>
2024-10-21 01:56:44 +0200

    short code so only one commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let price = parseFloat(prompt("What is the price of the product you'd like to purchase?"))
+let discountedPrice = price * 0.90;
+console.log('$' + discountedPrice.toFixed(2));
+// new price $49.99
