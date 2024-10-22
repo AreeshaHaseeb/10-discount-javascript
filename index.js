7d934127f2717f43e4af6aecc70d6a82b64d267f
Areesha Haseeb Khan Suontausta <32227763+AreeshaHaseeb@users.noreply.github.com>
2024-10-22 23:48:16 +0200

    added some improvments

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let price = parseFloat(prompt("What is the price of the product you'd like to purchase?"))
-let discountedPrice = price * 0.90;
+let priceInput = prompt("What is the price of the product you'd like to purchase?")
+let discountedPrice = parseFloat(priceInput.replace(/[^0-9\.-]+/g, "")) * 0.90;
 console.log('$' + discountedPrice.toFixed(2));
-// new price $49.99
